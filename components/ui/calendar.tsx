9d12de0509e8c9963c8edb062ce565653ed3de67
lakshitaa4 <lakshita0000001@gmail.com>
2025-09-05 19:53:36 +0530

    fix: update react-day-picker icon props in calendar component

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -52,8 +52,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
+        icon_left: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
+        icon_right: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
