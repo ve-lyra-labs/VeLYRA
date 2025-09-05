da32cd2fc0f1b7511c89df2b19574dbfa677a144
ve-lyra-labs <lyra.xai7@gmail.com>
2025-09-05 22:27:39 +0530

    Updated for vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children} 
+          <Analytics />
+        </Providers>
       </body>
     </html>
   );
