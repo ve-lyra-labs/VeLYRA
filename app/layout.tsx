7b17940c437c69f9c0d3c8fb72f88b07fb182a90
ve-lyra-labs <lyra.xai7@gmail.com>
2025-09-06 22:46:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
-import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +19,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
-          {children} 
-          <Analytics />
+          {children}
         </Providers>
       </body>
     </html>
